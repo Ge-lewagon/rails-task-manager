d4974f09dbadab3a4a213febca94a0dda24dd391
Ge Li <lige.wh@gmail.com>
2023-12-07 19:56:31 +0800

    dec

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,21 @@
 Rails.application.routes.draw do
+  #read all - fetching all data records from tasks model
+  get "tasks", to:'tasks#index'
+
+  #create new tasks
+  get "tasks/new", to:'tasks#new', as: :new_task
+  post "tasks", to:"tasks#create"
+
+  #read one - you want to show 1 particular record
+  get "tasks/:id", to:"tasks#show", as: :task
+
+  #update
+  get "tasks/:id/edit", to:"tasks#edit", as: :edit_task
+  patch "tasks/:id", to:"tasks#update"
+
+  #delete
+  delete "tasks/:id", to:"tasks#destroy"
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
